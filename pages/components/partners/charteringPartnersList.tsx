0eee1dce0d7dc3a8a31f9a829250474630ad0250
alarafatsiddique <development@softeko.co>
2025-07-14 06:33:40 +0600

    images added

--- pages/components/partners/charteringPartnersList.tsx
+++ pages/components/partners/charteringPartnersList.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function CharteringPartnersList() {
   const { ref, inView } = useInView({
@@ -8,29 +10,123 @@ export default function CharteringPartnersList() {
   });
 
   const partners = [
-    "OLDENDORFF GMBH & CO. KG, LUEBECK, GERMANY",
-    "TONGLI SHIPPING PTE. LTD.",
-    "MULTIMAX SHIPPING PTE. LTD.",
-    "LYNUX SHIPPING LIMITED",
-    "NORVIC SHIPPING ASIA PTE LTD",
-    "CRYSTAL SEA SHIPPING CO LTD",
-    "AGROCORP INTERNATIONAL PTE LTD SINGAPORE",
-    "SIGMA SHIPPING INTERNATIONAL S.A.",
-    "OILMAR (DREAM SEA NAVIGATION",
-    "PROPEL SHIPPING PTE LTD",
-    "VIETSEA COMPANY PTE LTD",
-    "SILK ROAD SHIPPING & CHARTERING LTD",
-    "GUARDIAN BULK SHIPPING CO.,LTD.",
-    "CAPITAL WORLD MARITIME LIMITED",
-    "CAMBRIAN BULK LIMITED",
-    "LOCH SHIPPING INTERMEDIARY FZE, U.A.E",
-    "ALLIANZ BULK CFR8 PTE. LTD.",
-    "TEAM BULK CARRIERS PTE LTD , SINGAPORE",
-    "BAINBRIDGE NAVIGATION DMCC",
-    "M/S. JAHAZ SHIPPING LINES DMCC",
-    "BULK ASIA PTE LTD",
-    "WEALTH CREATION GENERAL TRADING LLC",
-    "EVER GLORY SHIP CHARTER LLC",
+    {
+      title: "OLDENDORFF GMBH & CO. KG, LUEBECK, GERMANY",
+      logoUrl: "/assets/images/partners/oldendorff-logo.png",
+      siteUrl: "https://www.oldendorff.com/",
+    },
+    {
+      title: "TONGLI SHIPPING PTE. LTD.",
+      logoUrl: "/assets/images/partners/TongLiLogo_Coloured.png",
+      siteUrl: "https://tongli-shipping.com/",
+    },
+    {
+      title: "MULTIMAX SHIPPING PTE. LTD.",
+      logoUrl: "/assets/images/partners/multimax-logo.png",
+      siteUrl: "https://www.multimax.com/",
+    },
+    {
+      title: "LYNUX SHIPPING LIMITED",
+      logoUrl: "/assets/images/partners/Linx2_02.jpg",
+      siteUrl: "https://www.lynxshipping.com/",
+    },
+    {
+      title: "NORVIC SHIPPING ASIA PTE LTD",
+      logoUrl: "/assets/images/partners/norvic.png",
+      siteUrl: "https://norvicshipping.com/",
+    },
+    {
+      title: "CRYSTAL SEA SHIPPING CO LTD",
+      logoUrl: "/assets/images/partners/Crystal_Logo_01.png",
+      siteUrl: "https://www.crystalshippinginc.com/",
+    },
+    {
+      title: "AGROCORP INTERNATIONAL PTE LTD SINGAPORE",
+      logoUrl: "/assets/images/partners/agrocorp-logo.png",
+      siteUrl: "https://agrocorp.com.sg/",
+    },
+    {
+      title: "SIGMA SHIPPING INTERNATIONAL S.A.",
+      logoUrl: "/assets/images/partners/sigma-shipping.jpg",
+      siteUrl: "https://www.sigma-shipping.com/",
+    },
+    {
+      title: "OILMAR (DREAM SEA NAVIGATION)",
+      logoUrl: "/assets/images/partners/olimar.png",
+      siteUrl: "https://dreamseanavigation.com/",
+    },
+    {
+      title: "PROPEL SHIPPING PTE LTD",
+      logoUrl: "/assets/images/partners/propel.png",
+      siteUrl: "https://www.propelship.com/",
+    },
+    {
+      title: "VIETSEA COMPANY PTE LTD",
+      logoUrl: "/assets/images/partners/vietsea.png",
+      siteUrl: "https://www.vietsea.net/",
+    },
+    {
+      title: "SILK ROAD SHIPPING & CHARTERING LTD",
+      logoUrl: "/assets/images/partners/silkroadlogo.png",
+      siteUrl: "https://www.silkgroup.com.jo/",
+    },
+
+    // done
+    {
+      title: "GUARDIAN BULK SHIPPING CO.,LTD.",
+      logoUrl: "/assets/images/partners/guardina.jpg",
+      siteUrl: "http://www.guardianintlshipping.com/",
+    },
+    {
+      title: "CAPITAL WORLD MARITIME LIMITED",
+      logoUrl: "/assets/images/partners/capital-world.png",
+      siteUrl: "https://ssgil.com/",
+    },
+    {
+      title: "CAMBRIAN BULK LIMITED",
+      logoUrl: "/public/assets/images/partners/cambrian.png",
+      siteUrl: "https://www.cambrian.com/",
+    },
+    {
+      title: "LOCH SHIPPING INTERMEDIARY FZE, U.A.E",
+      logoUrl: "/public/assets/images/partners/loch.png",
+      siteUrl: "https://www.lochshipping.com/",
+    },
+    {
+      title: "ALLIANZ BULK CFR8 PTE. LTD.",
+      logoUrl: "/public/assets/images/partners/allianz.png",
+      siteUrl: "https://www.allianz.com/",
+    },
+    {
+      title: "TEAM BULK CARRIERS PTE LTD , SINGAPORE",
+      logoUrl: "/assets/images/partners/team.png",
+      siteUrl: "https://www.team.com/",
+    },
+    {
+      title: "BAINBRIDGE NAVIGATION DMCC",
+      logoUrl: "/assets/images/partners/bainbridge.png",
+      siteUrl: "https://www.bainbridge.com/",
+    },
+    {
+      title: "M/S. JAHAZ SHIPPING LINES DMCC",
+      logoUrl: "/assets/images/partners/jahaz.png",
+      siteUrl: "https://www.jahaz.com/",
+    },
+    {
+      title: "BULK ASIA PTE LTD",
+      logoUrl: "/assets/images/partners/bulkasia.png",
+      siteUrl: "https://www.bulkasia.com/",
+    },
+    {
+      title: "WEALTH CREATION GENERAL TRADING LLC",
+      logoUrl: "/assets/images/partners/wealth.png",
+      siteUrl: "https://www.wealthcreation.com/",
+    },
+    {
+      title: "EVER GLORY SHIP CHARTER LLC",
+      logoUrl: "/assets/images/partners/everglory.png",
+      siteUrl: "https://www.everglory.com/",
+    },
   ];
 
   return (
@@ -39,7 +135,7 @@ export default function CharteringPartnersList() {
       initial={{ opacity: 0 }}
       animate={inView ? { opacity: 1 } : { opacity: 0 }}
       transition={{ duration: 0.6 }}
-      className="bg-gradient-to-br from-slate-900 to-slate-800 p-8 rounded-2xl shadow-xl border border-slate-700/30 backdrop-blur-sm"
+      className=" p-4 rounded-xl shadow-xl border border-slate-700/30 backdrop-blur-sm"
     >
       <h3 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-cyan-300 mb-6">
         Chartering Partners
@@ -49,9 +145,11 @@ export default function CharteringPartnersList() {
         {partners.map((partner, index) => (
           <Partner
             key={index}
-            title={partner}
+            title={partner.title}
             delay={index * 0.05}
             inView={inView}
+            logoUrl={partner.logoUrl}
+            siteUrl={partner.siteUrl}
           />
         ))}
       </div>
@@ -63,40 +161,44 @@ const Partner = ({
   title,
   delay,
   inView,
+  logoUrl,
+  siteUrl,
 }: {
   title: string;
   delay: number;
   inView: boolean;
+  logoUrl?: string;
+  siteUrl?: string;
 }) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
       transition={{ duration: 0.4, delay }}
-      className="flex items-center gap-3 rounded-lg hover:bg-slate-700/30 transition-colors"
+      className="flex items-center gap-3 rounded-lg hover:bg-gray-200 transition-colors"
     >
-      <div className="rounded-full bg-blue-500/20 p-2 flex items-center justify-center">
-        <CheckIcon />
-      </div>
-      <p className="text-slate-100 font-medium">{title}</p>
+      <Link
+        href={siteUrl || "/partners"}
+        className="flex items-center gap-3 p-4"
+        target="_blank"
+      >
+        <Logo logoUrl={logoUrl} />
+        <span className="text-lg font-semibold text-gray-800 no-underline">
+          {title}
+        </span>
+      </Link>
     </motion.div>
   );
 };
 
-const CheckIcon = () => {
+const Logo = ({ logoUrl }: { logoUrl?: string }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      className="text-blue-400"
-    >
-      <path fill="none" d="M0 0h24v24H0z" />
-      <path
-        fill="currentColor"
-        d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"
-      />
-    </svg>
+    <Image
+      src={logoUrl || "/default-logo.png"} // Fallback to a default logo if logoUrl is not provided
+      alt="Company Logo"
+      width={150}
+      height={150}
+      className="object-contain"
+    />
   );
 };
